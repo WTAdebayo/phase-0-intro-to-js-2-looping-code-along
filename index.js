887957a41a452b1a417ee6df971304aa2c6ca137
Adewole Adebayo <wole1105@gmail.com>
2022-05-27 15:14:49 -0700

    commit tree 5a56c772d915a608013a398f58fdf2d83f7e09d4

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let newNamesArray = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        newNamesArray.push(message)
+    }
+    return newNamesArray
+}
+
+function countDown() {
+let countDown = 10
+while (countDown > -1) {
+    console.log(countDown--)
+}
+}
